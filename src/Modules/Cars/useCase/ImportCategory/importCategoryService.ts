57b8debd67361c764dd0cda7c743f8d7a2eab9a9
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-08-09 18:31:21 -0300

    Criando e testando serviço para importação de categorias

--- src/Modules/Cars/useCase/ImportCategory/importCategoryService.ts
+++ src/Modules/Cars/useCase/ImportCategory/importCategoryService.ts
@@ -1,6 +1,51 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
+import { ICategoryRepository } from '../../Repositories/ICategoryRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class importCategoryService {
-  execute(file: unknown) {
-    console.log(file);
+  repository: ICategoryRepository;
+  categoryArray: Array<IImportCategory>;
+
+  constructor(repository: ICategoryRepository) {
+    this.repository = repository;
+    this.categoryArray = [];
+  }
+
+  loadCategory(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', (line) => {
+          const [name, description] = line;
+          this.categoryArray.push({ name, description });
+        })
+        .on('end', () => {
+          resolve(this.categoryArray);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategory(file);
+    categories.forEach(({ name, description }) => {
+      const findName = this.repository.findByName(name);
+      if (!findName) {
+        this.repository.create({ name, description });
+      }
+    });
   }
 }
 
--- src/Modules/Cars/useCase/ImportCategory/index.ts
+++ src/Modules/Cars/useCase/ImportCategory/index.ts
@@ -1,8 +1,10 @@
+import CategoryRepositoriy from '../../Repositories/implementations/CategoryRepository';
 import importCategoryController from './importCategoryController';
 import importCategoryService from './importCategoryService';
 
+const repository = CategoryRepositoriy.getInstace();
 // eslint-disable-next-line new-cap
-const importService = new importCategoryService();
+const importService = new importCategoryService(repository);
 // eslint-disable-next-line new-cap
 const importController = new importCategoryController(importService);
 
